51ddcf2bd222c9e070843c9b4294f8457b0e99e3
unknown <p1201072@IUTAINFOPC141.univ-lyon1.fr>
2013-11-27 11:25:34 +0100

    modif pour tester

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,8 +1,30 @@
 package mastermind;
+import java.io.*;
+import java.util.Random;
 
 public class Mastermind {
+    tab[] proposersolution(marqueur[], cpt)
+    {
+        if (cpt==0)
+        { 
+            for (int i=0; i<)
+        }
+        else
+	//boucle pour chaque case
+	if (marque[i]==-1)
+		//couleur non présente
+		//a enlever des propositions
+	else if (marque[i]==0)
+		//couleur présente mais pas ici
+		//a enlever de cette proposition
+	else //marqueur a 1
+		//on garde le même nombre
 
+    return solution[]   
+    }
+    
     public static void main(String[] args) {
         
     }
 }
+xxxxxx
