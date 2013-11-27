d278e271acbb999eef1b5aa8d15fd5915d38262d
unknown <p1201072@IUTAINFOPC141.univ-lyon1.fr>
2013-11-27 11:23:36 +0100

    init du repo

---
+++ mastermind/Mastermind.java
@@ -0,0 +1,27 @@
+package mastermind;
+
+public class Mastermind {
+    tab[] proposersolution(marqueur[], cpt)
+    {
+        if (cpt==0)
+        { 
+	//toutes les valeurs du tab sont rand%6
+        }
+        else
+	//boucle pour chaque case
+	if (marque[i]==-1)
+		//couleur non présente
+		//a enlever des propositions
+	else if (marque[i]==0)
+		//couleur présente mais pas ici
+		//a enlever de cette proposition
+	else //marqueur a 1
+		//on garde le même nombre
+
+    return solution[]   
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ mastermind/Proposition.java
@@ -0,0 +1,23 @@
+package mastermind;
+import java.io.*;
+
+public class Proposition {
+    tab[] proposersolution(marqueur[], cpt)
+    {
+        if (cpt==0)
+        { 
+	//toutes les valeurs du tab sont rand%6
+else
+	//boucle pour chaque case
+	if (marque[i]==-1)
+		//couleur non présente
+		//a enlever des propositions
+	else if (marque[i]==0)
+		//couleur présente mais pas ici
+		//a enlever de cette proposition
+	else //marqueur a 1
+		//on garde le même nombre
+
+    return solution[]
+}
+}
